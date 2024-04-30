ba83596b3da23725f3b1b6e4f613a4e931a3dad5
gkstkdgkr <gkstkdgkr12@gmail.com>
2024-04-30 14:50:03 +0900

    scripting

--- js/card.js
+++ js/card.js
@@ -2,5 +2,5 @@ const button = document.querySelector('.flip-button');
 const card = document.querySelector('.card');
 
 button.addEventListener('click', () => {
-  card.classList.toggle('is-flipped');
+  card.classList.toggle('is-flipped'); // on off 시키는 느낌 toggle
 });
